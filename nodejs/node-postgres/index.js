e4ea151cc6a10def11d0fbf455ebe84505fd3ae7
syy25 <grandallus1982@gmail.com>
2025-02-18 22:13:43 +0800

    Wipe old structure

--- nodejs/node-postgres/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path: './.env.development' });
-
-const app = require('./server'); // Import the Express app
-
-// Start the server
-const port = process.env.APP_PORT;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
