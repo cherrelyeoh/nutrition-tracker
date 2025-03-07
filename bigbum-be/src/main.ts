57c50d518d1ccf8f57131f80add46be0c0ab0a3a
cherrelyeoh <cherrel.yeoh@gmail.com>
2025-03-07 23:43:42 +0800

    BE : Set Body Size Limit for allowing upload of image

--- bigbum-be/src/main.ts
+++ bigbum-be/src/main.ts
@@ -1,17 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Increase request body size limit (e.g., 50MB)
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
   const config = new DocumentBuilder()
     .setTitle('BigBum Document')
     .setDescription('BigBum API description')
     .setVersion('1.0')
     .addTag('BigBum')
     .build();
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
