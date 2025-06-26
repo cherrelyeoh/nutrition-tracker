c132b0b891f30c3598e2a12eb8d646976f5163ed
Foo Joo Wee <joowee.foo@microlink.com.my>
2025-06-26 10:01:30 +0800

    FE - Minor bug fix

--- frontend_flutter/lib/pages/UserOnboarding/onboard3.dart
+++ frontend_flutter/lib/pages/UserOnboarding/onboard3.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertest/pages/UserOnboarding/gender_onboarding.dart';
 import 'package:fluttertest/pages/UserOnboarding/age_weight_height_onboarding.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fluttertest/pages/UserOnboarding/onboard4.dart';
