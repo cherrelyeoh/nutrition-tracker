c7ff3b9d0ec17a6e83c73c061c57bcc6e9633516
LAPTOP-E115NPB6\JooWee <joowee.foo@abagofdreams.com>
2025-07-04 20:40:13 +0800

    Fixing syntax error

--- bigbum-be/src/config/datasource.ts
+++ bigbum-be/src/config/datasource.ts
@@ -1,19 +1,20 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 const config = {
-  host: 'ep-floral-bonus-a1rem7lk-pooler.ap-southeast-1.aws.neon.tech',
-  database: 'bigbum_db',
-  username: 'neondb_owner',
-  password: 'npg_POpdDS2B0lxT',
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  //   type: 'postgres',
+  type: 'postgres',
   // host: 'localhost',
   // port: 5432,
   // database: 'bigbum_db',
   // username: 'postgres',
   // password: 'admin',
+  host: 'ep-floral-bonus-a1rem7lk-pooler.ap-southeast-1.aws.neon.tech', // Neon host
+  port: 5432,
+  database: 'bigbum_db', // Neon database
+  username: 'neondb_owner', // Neon user
+  password: 'npg_POpdDS2B0lxT', // Neon password
+  ssl: {
+    rejectUnauthorized: false, // Required for Neon
+  },
   seeds: [__dirname + '/seeds/*{.ts,.js}'],
   entities: [__dirname + '/../modules/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
@@ -21,7 +22,7 @@ const config = {
   logging: process.env.TYPEORM_LOGGING === 'true',
   synchronize: false,
   // autoLoadEntities: true, // this is important
-} as unknown as DataSourceOptions;
+} as DataSourceOptions;
 
 const datasource = new DataSource(config);
 
